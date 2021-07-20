f0a1f24c4471952b2b25252f1b3513846c597911
zsx29 <zsx29@naver.com>
2021-07-20 14:21:25 +0900

    2021-07-20 Kmarket update

--- kmarket/src/main/java/kr/co/kmarket/controller/MainController.java
+++ kmarket/src/main/java/kr/co/kmarket/controller/MainController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class MainController {
 
-	@GetMapping(value = {"/", "index"})
+	@GetMapping(value={"/", "index"})
 	public String index() {
 		return "/index";
 	}
