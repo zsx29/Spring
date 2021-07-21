30a28d729183f5170a36dfdcc5ff80d931012479
zsx29 <zsx29@naver.com>
2021-07-21 14:38:57 +0900

    2021-07-21 Kmarket update

---
+++ Kmarket/src/main/java/kr/co/kmarket/vo/Cate1Vo.java
@@ -0,0 +1,11 @@
+package kr.co.kmarket.vo;
+
+import lombok.Data;
+
+@Data
+public class Cate1Vo {
+
+	private int code1;
+	private String title;
+	
+}
---
+++ Kmarket/src/main/java/kr/co/kmarket/vo/Cate2Vo.java
@@ -0,0 +1,13 @@
+package kr.co.kmarket.vo;
+
+import lombok.Data;
+
+@Data
+public class Cate2Vo {
+
+	private int seq;
+	private int code1;
+	private int code2;
+	private String title;
+	
+}
